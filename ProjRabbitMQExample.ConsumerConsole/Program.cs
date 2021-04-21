a65bc9d3eac4fa53b2cbc8b9076d0aabf37310ce
unknown <aannddrree>
2021-04-21 15:22:12 -0300

    implantacao

---
+++ ProjRabbitMQExample.ConsumerConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using ProjRabbitMQExample.Utils;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace ProjRabbitMQExample.ConsumerConsole
+{
+    class Program
+    {
+        private const string QUEUE_NAME = "message";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: QUEUE_NAME,
+                                  durable: false,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+                    while (true)
+                    {
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                        {
+                            var body = ea.Body.ToArray();
+                            var returnMessage = Encoding.UTF8.GetString(body);
+                            var message = JsonConvert.DeserializeObject<Message>(returnMessage);
+
+                            Console.WriteLine("Description: " + message.Description);
+                        };
+
+                        channel.BasicConsume(queue: QUEUE_NAME,
+                                             autoAck: true,
+                                             consumer: consumer);
+
+                        Thread.Sleep(2000);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProjRabbitMQExample.Producer/Controllers/MessageProducerController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+using System.Text;
+using ProjRabbitMQExample.Utils;
+
+namespace ProjRabbitMQExample.Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageProducerController : ControllerBase
+    {
+        private readonly ConnectionFactory _factory;
+        private const string QUEUE_NAME = "message";
+
+        public MessageProducerController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] Message message)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments:null
+                        );
+
+                    var stringfieldMessage = JsonConvert.SerializeObject(message);
+                    var bytesMessage = Encoding.UTF8.GetBytes(stringfieldMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: bytesMessage
+                        );
+                }
+            }
+            return Accepted();
+        }
+    }
+}
